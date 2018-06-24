4bd040bed173d5a2d85d6dd38903a975d38f5ab4
Bryan Teng <bteng1226@gmail.com>
2018-06-24 23:18:56 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,6 @@ def my_hash
   my_hash= {"Plus" => "Ultra"}
 end
 
-
 def shipping_manifest
   the_manifest = {
                   "whale bone corsets" => 5, 
@@ -17,8 +16,6 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 shipping_manifest["oil paintings"]
 end
 
@@ -28,14 +25,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"] = 2
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
 shipping_manifest["gun powder"] = 4
 
-  # return the shipping_manifest hash below
 shipping_manifest
 end
